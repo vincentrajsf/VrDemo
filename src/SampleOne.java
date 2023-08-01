da9875d71288d187c6ab65a94d342680a28c1499
vincent.paulraj <vincent.paulraj@sourcefuse.com>
2023-08-01 18:25:40 +0530

    Test

---
+++ src/SampleOne.java
@@ -0,0 +1,7 @@
+public class SampleOne {
+    public static void main ( String []args){
+        int a=10, b=15, sum;
+          sum = a+b;
+          System.out.println("Sum of these number" + sum);
+    }
+}
